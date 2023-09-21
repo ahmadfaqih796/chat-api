a1673620b77e78523bcf26d51936022eaee1bef7
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-21 09:24:13 +0700

    add field name

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,32 +1,37 @@
 // See https://sequelize.org/master/manual/model-basics.html
 // for more of what you can do here.
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
-  const sequelizeClient = app.get('sequelizeClient');
-  const users = sequelizeClient.define('users', {
-  
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
+  const sequelizeClient = app.get("sequelizeClient");
+  const users = sequelizeClient.define(
+    "users",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      googleId: { type: DataTypes.STRING },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-  
-  
-    googleId: { type: DataTypes.STRING },
-  
-  }, {
-    hooks: {
-      beforeCount(options) {
-        options.raw = true;
-      }
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        },
+      },
     }
-  });
+  );
 
   // eslint-disable-next-line no-unused-vars
   users.associate = function (models) {
