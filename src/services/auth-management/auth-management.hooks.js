566e0f55bea74c452ddfe3de47ff1a85aeabc59f
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-25 09:18:06 +0700

    auth management hooks

--- src/services/auth-management/auth-management.hooks.js
+++ src/services/auth-management/auth-management.hooks.js
@@ -7,14 +7,59 @@ const {
   disallow,
 } = require("feathers-hooks-common");
 
+const isAction =
+  (...args) =>
+  (hook) =>
+    args.includes(hook.data.action);
+
+const logError = () => {
+  return async (context) => {
+    const { error } = context;
+    const { errors: err } = error;
+    const { $className } = err;
+
+    switch ($className) {
+      case "verifyExpired":
+        throw new errors.BadRequest(
+          "Kode OTP sudah kadaluarsa. Silahkan kirim ulang OTP"
+        );
+      case "badParam":
+        throw new errors.BadRequest(
+          "Kode OTP invalid. Silahkan kirim ulang OTP"
+        );
+      case "incorrectToken":
+        throw new errors.BadRequest("Kode reset password invalid.");
+      default:
+        break;
+    }
+
+    return context;
+  };
+};
+
 module.exports = {
   before: {
-    all: [authenticate("jwt")],
+    all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [
+      iff(isAction("passwordChange", "identityChange"), [authenticate("jwt")]),
+    ],
+    update: [disallow("external")],
+    patch: [
+      iff(
+        isProvider("external"),
+        preventChanges(
+          true,
+          "email",
+          "isVerified",
+          "verifyToken",
+          "verifyExpires",
+          "resetToken",
+          "resetExpires"
+        )
+      ),
+    ],
     remove: [],
   },
 
@@ -32,7 +77,7 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [logError()],
     update: [],
     patch: [],
     remove: [],
