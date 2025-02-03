45f6d4a3b3b4fb6d4c4b88274a9d5e62fb5a24cd
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-02-03 13:17:11 +0700

    message before create hook

--- src/services/messages/messages.hooks.js
+++ src/services/messages/messages.hooks.js
@@ -3,12 +3,24 @@ const { disablePagination } = require("feathers-hooks-common");
 const includeUser = require("../../hooks/includes/include-user");
 const includeMessageStatus = require("../../hooks/includes/include-message-status");
 
+const handleBeforeCreate = () => {
+  return async (context) => {
+    const { data } = context;
+    const { user } = context.params;
+    if (user) {
+      data.sender_id = user.id;
+      data.is_read = false;
+    }
+    return context;
+  };
+};
+
 module.exports = {
   before: {
     all: [authenticate("jwt")],
     find: [disablePagination(), includeMessageStatus()],
     get: [],
-    create: [],
+    create: [handleBeforeCreate()],
     update: [],
     patch: [],
     remove: [],
